259cf271d38fc15a1e5374da8768a0d3843a3215
Bobby Chien <bobby.chien@gmail.com>
2017-08-23 14:45:58 +0800

    Judge Route Circle (1 line)

--- judge-route-circle.py
+++ judge-route-circle.py
@@ -21,17 +21,7 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        x = y = 0
-        for move in moves:
-            if move == 'U':
-                y += 1
-            elif move == 'D':
-                y -= 1
-            elif move == 'L':
-                x -= 1
-            elif move == 'R':
-                x += 1
-        return ( x == 0 and y == 0)
+        return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
 
 if __name__ == '__main__':
     unittest.main()
